3431e99d9701cca21062c44dc7c75e5db800e880
AdrienHalletUCL <adrien.hallet@student.uclouvain.be>
2018-02-23 10:19:41 +0100

    First batch of comments

--- solution.pl
+++ solution.pl
@@ -1,5 +1,3 @@
-% We begin our Great Adventure !
-
 /* build_error_message(+Type, +Var, -Message)
  * Constructs an error message of the given @Type
  * With the possible help of the information @Var
@@ -50,32 +48,52 @@ assert_table_exists(Table) :-
 /* index_of(+Columns, +Column, -Index)
  * Get the @Index of the @Column inside @Columns
  */
-index_of([Head|_], Column, 0) :- !.
+index_of([_|_], _, 0) :- !.
 index_of([_|Tail], Column, Index) :-
   index_of(Tail, Column, SubIndex),
   !,
   SubIndex is Index+1.
 
-replace(_, _, [], []).
-replace(O, R, [O|T], [R|T2]) :- replace(O, R, T, T2).
-replace(O, R, [H|T], [H|T2]) :- dif(H,O), replace(O, R, T, T2).
+/* replace(+Old, +New, +OldList, -NewList)
+ * Replace the first occurence of @Old by @New
+ * in @OldList and bind to @NewList
+ */
+replace(_, _, [], []).                      % base case
+replace(Old, New, [Old|T1], [New|T2]) :-    % Head of list is value
+    replace(Old, New, T1, T2).
+replace(Old, New, [H|T1], [H|T2]) :-        % Pursue the search
+    dif(H,Old),                             % Head is not the value
+    replace(Old, New, T1, T2).
 
+/* tables
+ * List the tables on prompt
+ */
 tables :-
     findall(X, table(X,_,_), Tables), % find all tables
-    print_elems(Tables).               % display them
+    print_elems(Tables).              % display them
 
+/* tables(-Tables)
+ * Bind the list of tables to @Tables
+ */
 tables(Tables) :-
     findall(X, table(X,_,_), Tables).
 
+/* assert_full_col_name(+Table, +Col, -VerifiedCol)
+ * Ensures that @VerifiedCol contains the full selector name
+ * of @Col inside @Table
+ */
 assert_full_col_name(Table, Col, VerifiedCol) :-
   (
-    X/Y = Col
+    _/_ = Col
     ->
-      VerifiedCol = Col
+      VerifiedCol = Col         % We already had the full name
     ;
-      VerifiedCol = Table/Col
+      VerifiedCol = Table/Col   % Get the full name
   ).
 
+/* create(+Table, +Cols)
+ * Adds the @Table with columns @Cols to the database
+ */
 create(Table, Cols) :-
     % Todo : check if two cols do not have the same name
     % Todo : check that Cols is not empty
@@ -91,6 +109,9 @@ create(Table, Cols) :-
         assert(table(Table, Cols, Length))
     ).
 
+/* cols(+Table, -Cols)
+ * Get the @Table's columns inside @Cols
+ */
 cols(Table, Cols) :-
     tables(Tables),
     (
@@ -103,6 +124,9 @@ cols(Table, Cols) :-
         throw(Message)
     ).
 
+/* rows(+Table)
+ * List the rows of @table in prompt
+ */
 rows(Table) :-
     % Todo : error message if table empty
     tables(Tables),
@@ -117,6 +141,9 @@ rows(Table) :-
         throw(Message)
     ).
 
+/* insert(+Table, +Row)
+ * Insert the @Row in @Table
+ */
 insert(Table, Row) :-
     % Check that we want to insert a list
     not(is_list(Row)),
@@ -131,8 +158,9 @@ insert(Table, Row) :-
         ->
             assert(row(Table, Row))
         ;
-        build_error_message(
-            missing_columns,
+        % You want to insert too few/many columns
+        build_error_message( 
+            missing_columns,      
             (Length, Found),
             Message),
         !, throw(Message)
@@ -141,11 +169,14 @@ insert(Table, Row) :-
 
     % Table does not exist
     build_error_message(no_table, Table, Message),
-    throw(Message).
+    !, throw(Message).
 
+/*
+ *
+ */
 drop(Table) :-
     (
-        member(Table, Tables)
+        member(Table, _)
         ->
         retractall(row(Table,_)),
         retract(table(Table,_,_))
@@ -153,20 +184,24 @@ drop(Table) :-
         build_error_message(no_table,Table,Message),
         !,throw(Message)
     ).
-
+/*
+ *
+ */
 delete(Table) :-
     (
-        member(Table,Tables)
+        member(Table,_)
         ->
         retractall(row(Table,_))
         ;
         build_error_message(no_table,Table,Message),
         !,throw(Message)
     ).
-
+/*
+ *
+ */
 delete(Table,Conds) :-
     (
-        member(Table,Tables)
+        member(Table,_)
         ->
         handle_conds(Conds,ListofArgs),
         retract(row(Table,ListofArgs))
@@ -175,11 +210,17 @@ delete(Table,Conds) :-
         !,throw(Message)
     ).
 
-handle_conds([],ListofArgs).
+/* handle_conds(+Conds, -ListofArgs)
+ *
+ */
+handle_conds([],_).
 handle_conds([H|T],ListofArgs) :-
     arg(2,H,V),
     handle_conds(T,[ListofArgs|V]).
 
+/* replace_plus(+TableName, +ToRemove, -Removed) %Todo: check duplicate with assert_...
+ *
+ */
 replace_plus(TableName, ToRemove, Removed) :-
 
   (ToRemove = +X
@@ -189,81 +230,73 @@ replace_plus(TableName, ToRemove, Removed) :-
   ToRemove = Removed
   ).
 
-table_columns(Table, Columns) :-
-  table(Table, Columns,_).
-
-/* Old version not accomodating un-tabled column names
-parse_selectors(TableOrTables, Selectors, ParsedSelectors) :-
-  (
-    TableOrTables = [Htable|Ttable]
-    ->
-      (
-        Selectors = [Hsel,Tpoivre]
-        ->
-          maplist(replace_plus(Htable), Selectors, ParsedSelectors)
-        ;
-          (
-            Selectors = +X
-            ->
-              ParsedSelectors = HTable/X
-            ;
-              maplist(table_columns, TableOrTables, TempSelectors),
-              flatten(TempSelectors, ParsedSelectors)
-          )
-      )
-    ;
-      (
-        is_list(Selectors)
-        ->
-          maplist(replace_plus(TableOrTables), Selectors, ParsedSelectors)
-        ;
-          table_columns(TableOrTables, ParsedSelectors)
-      )
-
-  ).*/
+/* parse_selectors(+Table(s),+Selector(s), -ParsedSelectors) 
+ * @Table(s)       : can be list of tables or single table
+ * @Selector(s)    : can be one or multiple selectors (also *)
+ * @ParsedSelectors: gets bound to the full selectors
+ *
+ * The parser takes all the possible (assumed) values that we can
+ * request on a selection, either with multiple or single selectors.
+ * @ParsedSelectors will then contain the full, standardized selectors
+ * to be used inside the selec predicate.
+ */
 parse_selectors([],Selectors, ParsedSelectors) :-
+  % We emptied the table list and we flatten it 
   !,flatten(Selectors, ParsedSelectors).
-parse_selectors([Htable|Ttable], [], ParsedSelectors).
-parse_selectors([Htable|Ttable], Selectors, ParsedSelectors) :-
+  
+parse_selectors([_|_], [], _).
+  % We emptied the table and selector lists
+  
+parse_selectors([_|_], Selectors, ParsedSelectors) :- %Todo check weirdness
     Selectors = +X,
-    ParsedSelectors = HTable/X.
+    ParsedSelectors = _/X.
 
-parse_selectors([Htable|Ttable], [HSelector|TSelector], ParsedSelectors) :-
+parse_selectors([Htable|_], [HSelector|TSelector], ParsedSelectors) :-
   maplist(replace_plus(Htable), [HSelector,TSelector], RawParsedSelectors),
   !,flatten(RawParsedSelectors,ParsedSelectors).
+  
 parse_selectors(Table, Selectors, ParsedSelectors) :-
   (
     is_list(Selectors)
     ->
       maplist(replace_plus(Table), Selectors, ParsedSelectors)
     ;
-      table_columns(Table, RawParsedSelectors),
+      cols(Table, RawParsedSelectors),
       maplist(assert_full_col_name(Table), RawParsedSelectors, ParsedSelectors)
   ).
 
-
+/* selec(+TableOrTables, +Selectors, +Conds, -Projection)
+ * @TableOrTables   : The table(s) to select
+ * @Selectors       : The column(s) to select
+ * @Conds           : The conditions to apply on our selection
+ * @Projection      : The results of the selection
+ */
 selec(TableOrTables, Selectors, Conds, Projection) :-
   parse_selectors(TableOrTables, Selectors, ParsedSelectors),
-  %Conds = [H|T],
-  %!, selec_one(TableOrTables, Selectors, H, Projection);
-  !, selec_one(TableOrTables, ParsedSelectors, Conds, Projection).
-  %!, selec_whole_table(TableOrTables, Projection). %Todo Remove
+  !, selec_worker(TableOrTables, ParsedSelectors, Conds, Projection).
 
-%Currently Working for only one table and condition, selector still useless
-selec_one(Table, Selectors, Conds, Projection) :-
+/* selec_worker(+Table(s), +Selector(s), +Cond(s), -Projection)
+ * @Table(s)    : The table(s) to select
+ * @Select(s)   : The column(s) to select, formatted
+ * @Cond(s)     : The conditions to apply on our selection
+ * @Projection  : The results of the selection
+ *
+ * The worker of the selection. It can be used instead of selec iif
+ * @Selectors is standardized by the parser
+ */
+selec_worker(Table, Selectors, Conds, Projection) :-
   (
-    Table = [H|T]
-    ->
-      maplist(table_columns, Table, TempColumns),
-      flatten(TempColumns,Columns),
-      maplist(row, [persons,cities],TempRow),
-      flatten(TempRow, Row)
-    ;
+    Table = [_|_]
+    -> % Multi-tables selection
+      maplist(cols, Table, TempColumns),
+      flatten(TempColumns,Columns),           %Get all viewed columns
+      maplist(row, [persons,cities],TempRow), %X-join the tables
+      flatten(TempRow, Row)                   %Get the rows
+    ; % Single-table selection
       table(Table, RawColumns,_),
       maplist(assert_full_col_name(Table),RawColumns,Columns),
       row(Table, Row)
   ),
-  %row(Table, Row), %Get row
 
   condition_loop(Conds, Columns, Row),
   (is_list(Selectors)
@@ -280,7 +313,7 @@ condition_loop([H|T], Columns, Row) :-
   condition_loop([First,Remain], Columns, Row);
 
   H=..CondList, %Transform the condition into a standard representation
-  replace(+X,Table/X,CondList,OrderList), %Convert the '+' notation to the standard table/column
+  replace(+X,_/X,CondList,OrderList), %Convert the '+' notation to the standard table/column
   OrderList = [Operator, Column, ExpectedValue],
   nth0(ColumnIndex, Columns, Column),
   nth0(ColumnIndex, Row, FoundValue),
